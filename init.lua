cdecb4ee8aa4988648da2909f8d454dce98a0e0e
VunixUser <krarhader748@gmail.com>
2026-06-18 14:17:37 +0300

    Update PandaUI to enhance window creation functionality
    Refactor PandaUI module to improve window creation.

--- init.lua
+++ init.lua
@@ -1,12 +1,9 @@
--- [[ PandaUI | Entry Point Module ]]
 local PandaUI = {}
 
--- استدعاء المجلدات الفرعية
 local Modules = script:WaitForChild("Modules")
 local Components = script:WaitForChild("Components")
 local ElementsFolder = Modules:WaitForChild("Elements")
 
--- ربط الملفات ببعضها
 local Theme = require(Modules:WaitForChild("Theme"))
 local Utils = require(Modules:WaitForChild("Utilities"))
 local Draggable = require(Modules:WaitForChild("Draggable"))
@@ -14,6 +11,7 @@ local WindowModule = require(Components:WaitForChild("Window"))
 local TabModule = require(Components:WaitForChild("Tab"))
 
 function PandaUI:CreateWindow(Config)
+    -- هنا يحدث الدمج الأكبر: نمرر كل شيء للنافذة
     return WindowModule.new(Config, Theme, Utils, Draggable, TabModule, ElementsFolder)
 end
 
