4668a90e13a6e0e0ef2ce4546c7ecd6343237992
VunixUser <krarhader748@gmail.com>
2026-06-18 13:52:48 +0300

    Implement draggable input module for UI

--- Modules/Draggable.lua
+++ Modules/Draggable.lua
@@ -1,1 +1,27 @@
+-- [[ PandaUI | Input Dragging Module ]]
+local UserInputService = game:GetService("UserInputService")
 
+return function(Main, TopBar)
+    local dragging, dragStart, startPos
+    
+    TopBar.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
+            dragging = true
+            dragStart = input.Position
+            startPos = Main.Position
+        end
+    end)
+    
+    TopBar.InputEnded:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then 
+            dragging = false 
+        end
+    end)
+    
+    UserInputService.InputChanged:Connect(function(input)
+        if dragging and input.UserInputType == Enum.UserInputType.MouseMovement then
+            local delta = input.Position - dragStart
+            Main.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+        end
+    end)
+end
